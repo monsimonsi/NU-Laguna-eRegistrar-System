44b267e5e1fca9ed6f0da59bed861eb1603d7424
Byron Caesar Capulong <capulongba@students.nu-laguna.edu.ph>
2026-04-11 04:36:10 +0800

    Login + Register Page
    - kulang nalang is yung programs under its dropdown menu and maybe other dropdown menu options

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 import AlumniRegistration from './pages/AlumniRegistration';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AlumniRegistration />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/alumni-registration" element={<AlumniRegistration />} />
       </Routes>
     </BrowserRouter>
   );
